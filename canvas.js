71fba2473ccc90e8ca909eaa4923e59113d7ac9f
nicoryo <nicoryo.giogio@gmail.com>
2021-10-16 15:03:53 +0900

    Change: Full viewer mode

--- canvas.js
+++ canvas.js
@@ -1,33 +1,44 @@
-onload = function outputVideo2(){
-  var video = document.getElementById("remote_video");
+//出力先
+const output = document.querySelector('#output');
+//マウス移動時
+document.addEventListener('onmousemove', onmousemove);
+onmousemove = function(e) {
+  output.innerHTML = `x:` + e.pageX + ` y:` + e.pageY;
+}
+//マウス離脱時
+document.addEventListener('onmouseout', onmouseout);
+
+onmouseout = function(e) {
+  output.innerHTML = ``;
+}
+
+onload = function(){
+  let video = document.getElementById('remote_video');
+  let canvas = document.getElementById("my-canvas");
+  let videoWidth = 0;
+  let videoHeight = 0;
+  let canvasWidth = document.documentElement.clientWidth;
+  let canvasHeight = document.documentElement.clientHeight;
+  let scale = 0;
+  let shiftX = 0;
+  const FRAMERATE = 1000/30;
+  let ctx = canvas.getContext('2d');
   video.style.display ="none";
+  video.style.minWidth = "100%";
+  video.style.minHeight = "100%";
+
  
   setInterval(function(){
-    var canvas = document.getElementById("my-canvas");
+    scale = canvasHeight / videoHeight;
+    shiftX = (canvasWidth - videoWidth*scale) / 2;
+    
     canvas.style.display ="block";
-    var ctx = canvas.getContext('2d');
-	  ctx.drawImage(video, 0, 0, 1280, 720);
-  //  ctx.globalAlpha = 0.5;
-  //  ctx.fillStyle = "rgb(0, 0, 255)";
-  //  ctx.fillRect(20,20,50,50);
-    ctx.beginPath();
-    ctx.globalAlpha = 0.5;
-    ctx.fillStyle = "rgb(0, 0, 255)";
-    ctx.moveTo(600, 720);
-    ctx.lineTo(640, 180);
-    ctx.lineTo(680, 720);
-    ctx.fill();
 
-    for(let i = 0; i < 5; i++) {
-      lineY = 3 / (i + 4) * 720;
-      ctx.beginPath();
-      ctx.globalAlpha = 0.5;
-      ctx.fillStyle = "rgb(0, 0, 255)";
-      ctx.moveTo(600, lineY );
-      ctx.lineTo(680, lineY);
-      ctx.lineWidth = 5 - i;
-      ctx.stroke();
+	  ctx.drawImage(video, shiftX, 0, videoWidth*scale, videoHeight*scale);
+    if ( video.readyState >= HTMLMediaElement.HAVE_METADATA ) {
+      videoWidth = video.videoWidth ;
+      videoHeight = video.videoHeight ;
     }
-  }, 1000/30);
+  }, FRAMERATE);
 }
 
